e5f2244c04d42931da386b916c8f22a65ebce94a
cgl <cgl@cgldeMacBook-Pro.local>
2022-01-06 23:23:35 +0800

    torch lib build

---
+++ cpp/helloClion/cmake-build-debug/CMakeFiles/helloClion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloClion.dir/main.cpp.o"
+  "helloClion"
+  "helloClion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
